3e2ed79a6aa9a28b6f1b7c2a2463461edea38526
Abhay Shukla <abhay.shukla04@gmail.com>
2016-09-09 09:05:30 +0530

    First commit

---
+++ hbase-api/src/main/java/com/example/myproject/App.java
@@ -0,0 +1,23 @@
+package com.example.myproject;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Hello world!
+ *
+ */
+@RestController
+@EnableAutoConfiguration
+public class App 
+{
+	@RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(App.class, args);
+    }
+}
